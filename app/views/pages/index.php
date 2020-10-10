eb500e90054f2a51de73a29f73b5630128c51aca
Zbigniew Szafrański <zbigniew.szafranski@gmail.com>
2020-10-10 22:55:31 +0200

    last

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,4 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 	<h1><?php echo $data['title'];?> </h1>
+    <p>This is framework to create Register and Login system. It is based on course from Udemy Traversy media.</p>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
